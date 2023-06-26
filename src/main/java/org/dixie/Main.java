4f0dfec50bec020870003e8263a914e75a607a17
Dixie Dean <d.w.sokolov26@gmail.com>
2023-06-26 12:13:04 +0300

    add pictures and adjust main

--- src/main/java/org/dixie/Main.java
+++ src/main/java/org/dixie/Main.java
@@ -1,7 +1,17 @@
 package org.dixie;
 
+import org.dixie.image.ColourSchemaChar;
+import org.dixie.image.Converter;
+import org.dixie.image.TextGraphicsConverter;
+
 public class Main {
     public static void main(String[] args) {
+        TextGraphicsConverter converter = new Converter(new ColourSchemaChar());
+        converter.convert();
 
+
+//        String url =
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
     }
 }
