3244ad4d656f9ee36217c328950d45fa256f23d5
Dixie Dean <d.w.sokolov26@gmail.com>
2023-06-26 11:59:03 +0300

    Add Converter

---
+++ src/main/java/org/dixie/image/Converter.java
@@ -0,0 +1,141 @@
+package org.dixie.image;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.List;
+
+public class Converter implements TextGraphicsConverter {
+    private int maxWidth;
+    private int maxHeight;
+    private double maxRatio;
+    private TextColorSchema schema;
+
+    public Converter(TextColorSchema schema) {
+        this.schema = schema;
+    }
+
+    @Override
+    public void setMaxWidth(int width) {
+        this.maxWidth = width;
+    }
+
+    @Override
+    public void setMaxHeight(int height) {
+        this.maxHeight = height;
+    }
+
+    @Override
+    public void setMaxRatio(double maxRatio) {
+        this.maxRatio = maxRatio;
+    }
+
+    @Override
+    public void setTextColorSchema(TextColorSchema schema) {
+        this.schema = schema;
+    }
+
+    @Override
+    public String convert(String url) throws IOException, ImageSizeException {
+        BufferedImage image = ImageIO.read(new URL(url));
+        if (checkAspectRatio(image)) {
+            HashMap<String, Integer> finalSize = determineNewSize(image);
+            Image scaledImg = changeImgSize(finalSize, image);
+            BufferedImage bwImg = makeImgBW(finalSize, scaledImg);
+            List<List<Character>> pixelsLists = collectPixelsToList(bwImg);
+            return printImgFromCharacters(pixelsLists);
+        }
+        return null;
+    }
+
+    private boolean checkAspectRatio(BufferedImage image) throws ImageSizeException {
+        double imgRatio = (double) image.getWidth() / image.getHeight();
+
+        if (imgRatio < maxRatio) {
+            return true;
+        } else {
+            throw new ImageSizeException(imgRatio, maxRatio);
+        }
+    }
+
+    private HashMap<String, Integer> determineNewSize(BufferedImage image) {
+        int shrinkInXTimes;
+
+        if (image.getWidth() > maxWidth && image.getHeight() > maxHeight) {
+            if (image.getHeight() > image.getWidth()) {
+                shrinkInXTimes = image.getHeight() / maxHeight;
+            } else {
+                shrinkInXTimes = image.getWidth() / maxWidth;
+            }
+            HashMap<String, Integer> newWidthAndHeight = new HashMap<>();
+            newWidthAndHeight.put("width", image.getWidth() / shrinkInXTimes);
+            newWidthAndHeight.put("height", image.getHeight() / shrinkInXTimes);
+            return newWidthAndHeight;
+
+        } else if (image.getWidth() > maxWidth && image.getHeight() < maxHeight) {
+            shrinkInXTimes = image.getWidth() / maxWidth;
+            HashMap<String, Integer> newWidthAndHeight = new HashMap<>();
+            newWidthAndHeight.put("width", image.getWidth() / shrinkInXTimes);
+            newWidthAndHeight.put("height", image.getHeight() / shrinkInXTimes);
+            return newWidthAndHeight;
+
+        } else if (image.getWidth() < maxWidth && image.getHeight() > maxHeight) {
+            shrinkInXTimes = image.getHeight() / maxHeight;
+            HashMap<String, Integer> newWidthAndHeight = new HashMap<>();
+            newWidthAndHeight.put("width", image.getWidth() / shrinkInXTimes);
+            newWidthAndHeight.put("height", image.getHeight() / shrinkInXTimes);
+            return newWidthAndHeight;
+
+        } else {
+            HashMap<String, Integer> newWidthAndHeight = new HashMap<>();
+            newWidthAndHeight.put("width", image.getWidth());
+            newWidthAndHeight.put("height", image.getHeight());
+            return newWidthAndHeight;
+        }
+    }
+
+    private Image changeImgSize(HashMap<String, Integer> size, BufferedImage image) {
+        return image.getScaledInstance(size.get("width"),
+                size.get("height"),
+                BufferedImage.SCALE_SMOOTH);
+    }
+
+    private BufferedImage makeImgBW(HashMap<String, Integer> size, Image scaledImage) {
+        BufferedImage bwImg = new BufferedImage(size.get("width"),
+                size.get("height"),
+                BufferedImage.TYPE_BYTE_GRAY);
+        Graphics2D graphics = bwImg.createGraphics();
+        graphics.drawImage(scaledImage, 0, 0, null);
+        return bwImg;
+    }
+
+    private List<List<Character>> collectPixelsToList(BufferedImage bwImg) {
+        WritableRaster bwRaster = bwImg.getRaster();
+        List<List<Character>> pixelsLists = new LinkedList<>();
+        for (int i = 0; i < bwRaster.getHeight(); i++) {
+            List<Character> certainPixelList = new LinkedList<>();
+            for (int j = 0; j < bwRaster.getWidth(); j++) {
+                int color = bwRaster.getPixel(j, i, new int[3])[0];
+                char c = schema.convert(color);
+                certainPixelList.add(c);
+            }
+            pixelsLists.add(certainPixelList);
+        }
+        return pixelsLists;
+    }
+
+    private String printImgFromCharacters(List<List<Character>> pixelsLists) {
+        StringBuilder result = new StringBuilder();
+        for (List<Character> listOfPixels : pixelsLists) {
+            for (char symbol : listOfPixels) {
+                result.append(symbol).append(symbol).append(symbol);
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+}
