74de20bed38f518cae919274d554041b48fb23c6
Dixie Dean <d.w.sokolov26@gmail.com>
2023-07-10 12:19:40 +0300

    Add new ColourSchemaChar

--- src/main/java/org/dixie/image/ColourSchemaChar.java
+++ src/main/java/org/dixie/image/ColourSchemaChar.java
@@ -1,10 +1,10 @@
 package org.dixie.image;
 
 public class ColourSchemaChar implements TextColorSchema {
-    private final char[] symbols = {'0', '&', '#', '+', '*', '=', '^', '\'', '.'};
+    private final char[] symbols = {'■', '▲', '¶', '®', 'Δ', '*', '•', '-'};
 
     @Override
     public char convert(int color) {
-        return symbols[(int) Math.floor(color / 256. * symbols.length)];
+        return symbols[(int) Math.floor(color / 256.0 * symbols.length)];
     }
 }
